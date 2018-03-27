1aebf21e234bef161bed921feb22b3fd4481eb1b
Martin-Alexander <martingianna@gmail.com>
2018-03-27 17:30:02 -0400

    parsing json in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require "json"
+require "byebug"
+
 User.destroy_all
 Region.destroy_all
 Retailer.destroy_all
@@ -8,3 +11,8 @@ Region.create(name: "Bordeaux")
 Retailer.create(name: "Château Primeur", category: "revendeur")
 Retailer.create(name: "Millesima", category: "revendeur")
 Retailer.create(name: "The Wine Merchang", category: "negociant")
+
+# bordeaux_primeurs_json = JSON.parse(File.open(Rails.root.join("db/scraper/bordeaux_primeurs.json")).read, symbolize_names: true)
+
+# bordeaux_primeurs_json[:wine_details].each do |wine_attributes|
+# end
