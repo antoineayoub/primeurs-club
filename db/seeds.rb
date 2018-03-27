31ce5665d27ff16d501c5460b9ccfac28048ae6b
Martin-Alexander <martingianna@gmail.com>
2018-03-27 13:38:52 -0400

    deleting previous users, regions, and retailers in the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Region.destroy_all
+Retailer.destroy_all
 
 User.create!(email: "antoine@ppc.com", password: "12345678", admin: true)
 
 Region.create(name: "Bordeaux")
-Retailer.create(name: "Château Primeur", type: "revendeur")
-Retailer.create(name: "Millesima", type: "revendeur")
-Retailer.create(name: "The Wine Merchang", type: "negociant")
+Retailer.create(name: "Château Primeur", category: "revendeur")
+Retailer.create(name: "Millesima", category: "revendeur")
+Retailer.create(name: "The Wine Merchang", category: "negociant")
