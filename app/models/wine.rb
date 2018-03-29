3ee5dc1d9034e34cf7e56fe2010148ea2f76a2d8
Martin-Alexander <martingianna@gmail.com>
2018-03-29 18:15:02 -0400

    added callback method to wine that generates its slug, and a validation to make sure it's unique

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,5 +1,11 @@
 class Wine < ApplicationRecord
+  include WineModules::StandardizeName
+
   belongs_to :appellation
   has_many :vintages
   has_many :photos
+
+  before_validation :standardize_name
+
+  validates_uniqueness_of :slug
 end
