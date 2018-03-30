529c219254e6fc16c6477dae73d810cf82476daf
Martin-Alexander <martingianna@gmail.com>
2018-03-30 15:41:37 -0400

    bord overview scraper now creates vendor records

--- lib/seed/bord_overview.rb
+++ lib/seed/bord_overview.rb
@@ -6,8 +6,12 @@ module Seed
 
     def initialize(number_of_wines)
       @json = load_json
+      @number_of_vendor_critics_created = 0
+      @number_of_vendor_vintages_created = 0
       build_array_of_wine_details(number_of_wines)
       run
+      Seed::Logger.info("number of vender critics created: #{@number_of_vendor_critics_created}")
+      Seed::Logger.info("number of vender vintages created: #{@number_of_vendor_vintages_created}")
     end
 
     private
@@ -21,28 +25,9 @@ module Seed
       @wine_details.each do |wine_attributes|
         begin
           appellation = Appellation.find_or_create_by(name: wine_attributes[:appellation])
-
-          wine = Wine.create!(
-            name: wine_attributes[:name],
-            rating: wine_attributes[:rating],
-            appellation: appellation
-          )
-
-          wine_attributes[:vintages].each do |vintage_attributes|
-            vintage = Vintage.create!(
-              vintage: vintage_attributes[:year],
-              price: format_vintage_price(vintage_attributes[:year]),
-              wine: wine
-            )
-
-            Seed::Logger.debug(vintage.as_json)
-
-            build_wine_notes_for_vintage(vintage, vintage_attributes)
-          end
-
-          Seed::Logger.info("wine created: #{wine.as_json}")
-          Seed::Logger.info("#{wine.vintages.map(&:wine_notes).count} wine notes added -- #{WineNote.count} total")
-          Seed::Logger.info("#{wine.vintages.count} vintages added -- #{Vintage.count} total")
+          wine = build_wine_with_appellation(appellation, wine_attributes)
+          build_vendor_vintages_for_wine(wine, wine_attributes)
+          log_wine_information(wine)
         rescue => e
           Seed::Logger.error(e)
         end
@@ -59,22 +44,71 @@ module Seed
       JSON.parse(File.open(file_path).read, symbolize_names: true)
     end
 
-    def timestamp_of_file(file_name)
-      file_name.split("/").last.split("_").first.to_i
+    def build_wine_with_appellation(appellation_object, wine_attributes)
+      attributes = {
+        website: website_name,
+        name: wine_attributes[:name],
+        rating: wine_attributes[:rating],
+        appellation: appellation_object
+      }
+
+      VendorWine.create!(attributes)
+    rescue ActiveRecord::RecordInvalid => e
+      Seed::Logger.warn(e)
     end
 
-    def build_wine_notes_for_vintage(vintage_object, vintage_attributes)
-      raters = [:RP, :NM, :JR, :TA, :"B&D", :JS, :JL, :De, :RVF, :JA, :LeP, :PW, :RG]
+    def build_vendor_vintages_for_wine(wine_object, wine_attributes)
+      wine_attributes[:vintages].each do |vintage_attributes|
+        begin
+          vintage = VendorVintage.create!(
+            website: website_name,
+            vintage: vintage_attributes[:year],
+            price_cents: format_vintage_price(vintage_attributes[:price]),
+            vendor_wine: wine_object
+          )
 
-      raters.each do |rater|
-        wine_note = WineNote.create!(
-          name: rater.to_s,
-          note: vintage_attributes[rater],
-          vintage: vintage_object
-        )
+          @number_of_vendor_vintages_created += 1
 
-        Seed::Logger.debug(wine_note.as_json)
+          Seed::Logger.debug(vintage.as_json)
+          build_vendor_critics_for_vintage(vintage, vintage_attributes)
+        rescue ActiveRecord::RecordInvalid => e
+          Seed::Logger.warn(e)
+        end
       end
+    end
+
+    def build_vendor_critics_for_vintage(vintage_object, vintage_attributes)
+      raters = [:RP, :NM, :JR, :TA, :"B&D", :JS, :JL, :De, :RVF, :JA, :LeP, :PW, :RG]
+      raters.each do |rater|
+        begin
+          wine_note = VendorCritic.create!(
+            website: website_name,
+            name: rater.to_s,
+            note: vintage_attributes[rater],
+            vendor_vintage: vintage_object
+          )
+
+          @number_of_vendor_critics_created += 1
+
+          Seed::Logger.debug(wine_note.as_json)
+        rescue ActiveRecord::RecordInvalid => e
+          Seed::Logger.warn(e)
+        end
+      end
+    end
+
+    def website_name
+      "bord overview"
+    end
+
+    def log_wine_information(wine_object)
+      Seed::Logger.debug("wine created: #{wine_object.as_json}")
+      Seed::Logger.debug("#{wine_object.vendor_vintages.map(&:vendor_critics).flatten.count} vendor_critics added")
+      Seed::Logger.debug("#{wine_object.vendor_vintages.count} vendor_vintages added")
+    end
+
+    def timestamp_of_file(file_name)
+      file_name.split("/").last.split("_").first.to_i
     end
 
     def format_vintage_price(price)
--- lib/seed/clean.rb
+++ lib/seed/clean.rb
@@ -1,18 +1,26 @@
 module Seed
   class Clean
     def self.run
-      Seed::Logger.info("destorying all #{User.count} users")
-      User.destroy_all
-      Seed::Logger.info("destorying all #{Region.count} regions")
-      Region.destroy_all
-      Seed::Logger.info("destorying all #{Retailer.count} retailers")
-      Retailer.destroy_all
+      Seed::Logger.info("destorying all #{VendorCritic.count} vendor critics")
+      VendorCritic.destroy_all
+      Seed::Logger.info("destorying all #{VendorVintage.count} vendor vintages")
+      VendorVintage.destroy_all
+      Seed::Logger.info("destorying all #{VendorWine.count} vendor wines")
+      VendorWine.destroy_all
       Seed::Logger.info("destorying all #{WineNote.count} wine notes")
       WineNote.destroy_all
       Seed::Logger.info("destorying all #{Vintage.count} vintages")
       Vintage.destroy_all
       Seed::Logger.info("destorying all #{Wine.count} wines")
       Wine.destroy_all
+      Seed::Logger.info("destorying all #{Appellation.count} appellations")
+      Appellation.destroy_all
+      Seed::Logger.info("destorying all #{User.count} users")
+      User.destroy_all
+      Seed::Logger.info("destorying all #{Region.count} regions")
+      Region.destroy_all
+      Seed::Logger.info("destorying all #{Retailer.count} retailers")
+      Retailer.destroy_all
     end
   end
 end
