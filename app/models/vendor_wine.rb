f64749c09dd1a45e0717ffef8a2319551d144abe
Martin-Alexander <martingianna@gmail.com>
2018-03-30 16:46:05 -0400

    added mechanism to prevent duplication of records

--- app/models/vendor_wine.rb
+++ app/models/vendor_wine.rb
@@ -2,4 +2,6 @@ class VendorWine < ApplicationRecord
   belongs_to :appellation
   belongs_to :region, optional: true
   has_many :vendor_vintages
+
+  validates :name, presence: true
 end
--- lib/seed/bord_overview.rb
+++ lib/seed/bord_overview.rb
@@ -10,14 +10,15 @@ module Seed
       @number_of_vendor_vintages_created = 0
       build_array_of_wine_details(number_of_wines)
       run
-      Seed::Logger.info("number of vender critics created: #{@number_of_vendor_critics_created}")
-      Seed::Logger.info("number of vender vintages created: #{@number_of_vendor_vintages_created}")
+      # Seed::Logger.info("number of vender critics created: #{@number_of_vendor_critics_created}")
+      # Seed::Logger.info("number of vender vintages created: #{@number_of_vendor_vintages_created}")
     end
 
     private
 
     def build_array_of_wine_details(number_of_wines)
-      @wine_details = number_of_wines ? @json[:wine_details].sample(number_of_wines.to_i) : @json[:wine_details]
+      # @wine_details = number_of_wines ? @json[:wine_details].sample(number_of_wines.to_i) : @json[:wine_details]
+      @wine_details = number_of_wines ? @json[:wine_details][0...number_of_wines.to_i] : @json[:wine_details]
       Seed::Logger.info("number of wines being seeded #{@wine_details.length}")
     end
 
@@ -27,7 +28,7 @@ module Seed
           appellation = Appellation.find_or_create_by(name: wine_attributes[:appellation])
           wine = build_wine_with_appellation(appellation, wine_attributes)
           build_vendor_vintages_for_wine(wine, wine_attributes)
-          log_wine_information(wine)
+          # log_wine_information(wine)
         rescue => e
           Seed::Logger.error(e)
         end
@@ -36,11 +37,10 @@ module Seed
 
     def load_json
       file_path = Dir.glob("#{Rails.root}/db/scraper/*_bord_overview.json").sort do |a, b|
-        timestamp_of_file(a) <=> timestamp_of_file(b)
+        timestamp_of_file(b) <=> timestamp_of_file(a)
       end.first
 
       Seed::Logger.info("initializing seed with: '#{file_path}'")
-
       JSON.parse(File.open(file_path).read, symbolize_names: true)
     end
 
@@ -52,48 +52,36 @@ module Seed
         appellation: appellation_object
       }
 
-      VendorWine.create!(attributes)
-    rescue ActiveRecord::RecordInvalid => e
-      Seed::Logger.warn(e)
+      VendorWine.conditionally_create(attributes, Seed::Logger)
     end
 
     def build_vendor_vintages_for_wine(wine_object, wine_attributes)
       wine_attributes[:vintages].each do |vintage_attributes|
-        begin
-          vintage = VendorVintage.create!(
-            website: website_name,
-            vintage: vintage_attributes[:year],
-            price_cents: format_vintage_price(vintage_attributes[:price]),
-            vendor_wine: wine_object
-          )
+        attributes = {
+          website: website_name,
+          vintage: vintage_attributes[:year],
+          price_cents: format_vintage_price(vintage_attributes[:price]),
+          vendor_wine: wine_object
+        }
 
-          @number_of_vendor_vintages_created += 1
-
-          Seed::Logger.debug(vintage.as_json)
-          build_vendor_critics_for_vintage(vintage, vintage_attributes)
-        rescue ActiveRecord::RecordInvalid => e
-          Seed::Logger.warn(e)
-        end
+        vintage = VendorVintage.conditionally_create(attributes, Seed::Logger)
+        # @number_of_vendor_vintages_created += 1 if vintage
+        build_vendor_critics_for_vintage(vintage, vintage_attributes)
       end
     end
 
     def build_vendor_critics_for_vintage(vintage_object, vintage_attributes)
       raters = [:RP, :NM, :JR, :TA, :"B&D", :JS, :JL, :De, :RVF, :JA, :LeP, :PW, :RG]
       raters.each do |rater|
-        begin
-          wine_note = VendorCritic.create!(
-            website: website_name,
-            name: rater.to_s,
-            note: vintage_attributes[rater],
-            vendor_vintage: vintage_object
-          )
+        attributes = {
+          website: website_name,
+          name: rater.to_s,
+          note: vintage_attributes[rater],
+          vendor_vintage: vintage_object
+        }
 
-          @number_of_vendor_critics_created += 1
-
-          Seed::Logger.debug(wine_note.as_json)
-        rescue ActiveRecord::RecordInvalid => e
-          Seed::Logger.warn(e)
-        end
+        critic = VendorCritic.conditionally_create(attributes, Seed::Logger)
+        # @number_of_vendor_critics_created += 1 if critic
       end
     end
 
@@ -101,11 +89,10 @@ module Seed
       "bord overview"
     end
 
-    def log_wine_information(wine_object)
-      Seed::Logger.debug("wine created: #{wine_object.as_json}")
-      Seed::Logger.debug("#{wine_object.vendor_vintages.map(&:vendor_critics).flatten.count} vendor_critics added")
-      Seed::Logger.debug("#{wine_object.vendor_vintages.count} vendor_vintages added")
-    end
+    # def log_wine_information(wine_object)
+    #   Seed::Logger.debug("#{wine_object.vendor_vintages.map(&:vendor_critics).flatten.count} vendor_critics added")
+    #   Seed::Logger.debug("#{wine_object.vendor_vintages.count} vendor_vintages added")
+    # end
 
     def timestamp_of_file(file_name)
       file_name.split("/").last.split("_").first.to_i
