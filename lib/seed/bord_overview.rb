bfeaad63446f503d0ba52b9038d5029d19373b01
Martin-Alexander <martingianna@gmail.com>
2018-03-30 14:28:49 -0400

    style fixes

--- lib/seed/bord_overview.rb
+++ lib/seed/bord_overview.rb
@@ -9,45 +9,45 @@ module Seed
       build_array_of_wine_details(number_of_wines)
       run
     end
-    
+
     private
-    
+
     def build_array_of_wine_details(number_of_wines)
       @wine_details = if number_of_wines
-        @json[:wine_details].sample(number_of_wines)
-      else
-        @json[:wine_details]
-      end
+                        @json[:wine_details].sample(number_of_wines)
+                      else
+                        @json[:wine_details]
+                      end
 
       Seed::Logger.info("number of wines being seeded #{number_of_wines}")
     end
-    
+
     def run
       @wine_details.each do |wine_attributes|
         begin
           appellation = Appellation.find_or_create_by(name: wine_attributes[:appellation])
-          
+
           wine = Wine.create!(
             name: wine_attributes[:name],
             rating: wine_attributes[:rating],
             appellation: appellation
           )
-  
+
           wine_attributes[:vintages].each do |vintage_attributes|
             vintage = Vintage.create!(
               vintage: vintage_attributes[:year],
               price: format_vintage_price(vintage_attributes[:year]),
               wine: wine
-              )
-              
-              Seed::Logger.debug(vintage.as_json)
-              
-              build_wine_notes_for_vintage(vintage, vintage_attributes)
-            end
-            
-            Seed::Logger.info("wine created: #{wine.as_json}")
-            Seed::Logger.info("#{wine.vintages.map(&:wine_notes).count} wine notes added -- #{WineNote.count} total")
-            Seed::Logger.info("#{wine.vintages.count} vintages added -- #{Vintage.count} total")
+            )
+
+            Seed::Logger.debug(vintage.as_json)
+
+            build_wine_notes_for_vintage(vintage, vintage_attributes)
+          end
+
+          Seed::Logger.info("wine created: #{wine.as_json}")
+          Seed::Logger.info("#{wine.vintages.map(&:wine_notes).count} wine notes added -- #{WineNote.count} total")
+          Seed::Logger.info("#{wine.vintages.count} vintages added -- #{Vintage.count} total")
         rescue => e
           Seed::Logger.error(e)
         end
@@ -74,6 +74,6 @@ module Seed
 
     def format_vintage_price(price)
       price.is_a?(String) ? price.gsub(/\D/, "") : nil
-    end    
+    end
   end
 end
