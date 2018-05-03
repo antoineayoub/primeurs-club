487ebf62626cdbf1363dc541c5dc246be898b9ff
Martin-Alexander <martingianna@gmail.com>
2018-05-03 16:08:50 -0400

    removed unneeded method

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,23 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  def self.create_unless_duplicate(attrs, logger = nil)
-    record = find_by(attrs) || new(attrs)
-
-    if record.persisted?
-      seed_log_duplicate
-    elsif record.save
-      logger&.debug("#{record.class} #{record.as_json}")
-    else
-      logger&.warn("#{record.class} is invalid: #{record.errors.messages}")
-    end
-
-    record
-  end
-
   def self.seed_log_duplicate
     Seed::Logger&.info("#{self}: Duplicate")
   end
+
+  private
 
   def log_results_of_save
     if persisted?
