878ad75196a2be125ba0caa8540fdec31f7aeebc
Martin-Alexander <martingianna@gmail.com>
2018-05-03 15:12:59 -0400

    refactored logging

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,13 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  # As of now this method only conditionally creates
-  # I don't yet know what the update conditions should be
-  def self.conditionally_create(attrs, logger = nil)
+  def self.create_unless_duplicate(attrs, logger = nil)
     record = find_by(attrs) || new(attrs)
 
     if record.persisted?
-      logger&.debug("Duplicate object detected")
+      seed_log_duplicate
     elsif record.save
       logger&.debug("#{record.class} #{record.as_json}")
     else
@@ -16,4 +14,16 @@ class ApplicationRecord < ActiveRecord::Base
 
     record
   end
+
+  def self.seed_log_duplicate
+    Seed::Logger&.info("#{self}: Duplicate")
+  end
+
+  def log_results_of_save
+    if persisted?
+      Seed::Logger&.debug("#{self.class}: #{self.as_json}")
+    else
+      Seed::Logger&.warn("#{self.class} is invalid: #{self.errors.messages}")
+    end
+  end    
 end
--- app/models/vendor_vintage.rb
+++ app/models/vendor_vintage.rb
@@ -1,11 +1,7 @@
-module VendorVintageClassMethods
-  def self.included(base)
-    base.extend(ClassMethods)
-  end
-
-  module ClassMethods
-    # If there exists a a VendorVintage of the same VendorWine and the same year then updates it's price if it's changed,
-    # otherwise if there is no existing, matching VendorVintage, create a new one
+class VendorVintage < ApplicationRecord
+  class VendorVintageError < StandardError; end
+  
+  module VendorVintageClassMethods
     def create_or_update_price(attrs)
       existing_vendor_vintage = find_by_wine_and_year(attrs)
       
@@ -13,18 +9,16 @@ module VendorVintageClassMethods
         Seed::Logger&.info("Updating #{existing_vendor_vintage.info_description} price from #{existing_vendor_vintage.price_cents} to #{attrs[:price_cents]}")
         existing_vendor_vintage.update(price_cents: attrs[:price_cents])
       elsif existing_vendor_vintage.nil?
-        new_vendor_vintage = new(attrs)
-        new_vendor_vintage.save
-        return new_vendor_vintage
+        return create(attrs)
       else
-        Seed::Logger&.debug("#{self}: Duplicate")
+        seed_log_duplicate
       end
     
       return existing_vendor_vintage
     end
     
     private
-
+  
     def find_by_wine_and_year(vendor_vintage_attrs)
       unless [:vendor_wine, :vintage].all? { |attribute| vendor_vintage_attrs.include?(attribute) }
         raise VendorVintageError, "Must include vendor_wine and vintage on vendor_vintage creation"
@@ -33,28 +27,19 @@ module VendorVintageClassMethods
       find_by(vendor_vintage_attrs.slice(:vendor_wine, :vintage))
     end
   end
-end
 
-class VendorVintage < ApplicationRecord
-  class VendorVintageError < StandardError; end
   include WineModules::StandardizeName
-  include VendorVintageClassMethods
+  extend VendorVintageClassMethods
 
   belongs_to :vendor_wine
   has_many :vendor_critics, dependent: :destroy
 
   validates_presence_of :vendor_wine
   
+  after_save :log_results_of_save
+
   def price_has_changed_from(price_cents)
     price_cents && self.price_cents != price_cents
-  end
-  
-  def save
-    if super
-      Seed::Logger&.debug("#{self.class}: #{self.as_json}")
-    else
-      Seed::Logger&.warn("#{self.class} is invalid: #{self.errors.messages}")
-    end
   end
 
   def info_description
