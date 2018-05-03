d1d26106b8410a371ccd5ba19770381cdb8a50b9
Martin-Alexander <martingianna@gmail.com>
2018-05-03 17:34:15 -0400

    proper naming convention

--- lib/scraper/global_wine_survey.rb
+++ lib/scraper/global_wine_survey.rb
@@ -1,6 +1,6 @@
 module Scraper
   class GlobalWineSurvey < Base
-    set_output_file "gws.json"
+    set_output_file "global_wine_survey.json"
 
     def run
       @output_hash = { wine_details: GWS.fetch_latest(region: "Bordeaux") }
