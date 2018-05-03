7c5a7bdb91f141f8056dd5e6aefa54db1a7b217f
Martin-Alexander <martingianna@gmail.com>
2018-05-03 18:13:02 -0400

    fixed scraper, such that it's output conformed to the expectation of the seeders

--- lib/scraper/global_wine_survey.rb
+++ lib/scraper/global_wine_survey.rb
@@ -3,7 +3,12 @@ module Scraper
     set_output_file "global_wine_survey.json"
 
     def run
-      @output_hash = { wine_details: GWS.fetch_latest(region: "Bordeaux") }
+      gws_results = GWS.fetch_latest(region: "Bordeaux").map do |wine_attribues|
+        wine_attribues["name"] = wine_attribues.delete("wine")
+        wine_attribues
+      end
+
+      @output_hash = { wine_details: gws_results }
     end
   end
 end
