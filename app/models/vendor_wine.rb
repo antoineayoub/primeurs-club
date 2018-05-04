0125f5856d8813543a621a9312379f36deefbff9
Martin-Alexander <martingianna@gmail.com>
2018-05-04 16:27:40 -0400

    fixed but in vendor wine

--- app/models/vendor_wine.rb
+++ app/models/vendor_wine.rb
@@ -14,15 +14,22 @@ class VendorWine < ApplicationRecord
   after_save :log_results_of_save
 
   def self.find_by_slug_or_create(attrs)
-    attrs[:wine] = Wine.find_by_slug_or_create(attrs)
-
-    found_vendor_wine = find_by(slug: attrs[:slug], website: attrs[:website])
-
-    if found_vendor_wine
+    if found_vendor_wine = find_by(slug: attrs[:slug], website: attrs[:website])
       seed_log_duplicate
       found_vendor_wine
     else
-      create!(attrs.slice(:name, :slug, :description, :wine, :website))
+      create_and_match_with_wine(attrs)
     end
+  end
+
+  def self.create_and_match_with_wine(wine_attrs)
+    wine_attrs[:slug] = Slug.generate(wine_attrs[:name], WineModules::StandardizeName::STANDARD_SLUG_METHODS)
+
+    wine = Wine.find_by_slug_or_create(wine_attrs)
+
+    vendor_wine = new(wine_attrs.slice(:name, :description, :website))
+    vendor_wine.wine = wine
+    vendor_wine.save!
+    vendor_wine
   end
 end
