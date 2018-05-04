6d023d055086cd82b4f2e62099f4ba8fdfcd1a41
Martin-Alexander <martingianna@gmail.com>
2018-05-04 15:49:03 -0400

    syncronized slug generation between Wine and VendorWine

--- app/models/vendor_wine.rb
+++ app/models/vendor_wine.rb
@@ -9,10 +9,11 @@ class VendorWine < ApplicationRecord
 
   validates :name, presence: true
 
+  before_create :standardize_name
+
   after_save :log_results_of_save
 
   def self.find_by_slug_or_create(attrs)
-    attrs[:slug] = Slug.generate(attrs[:name], WineModules::StandardizeName::STANDARD_SLUG_METHODS)
     attrs[:wine] = Wine.find_by_slug_or_create(attrs)
 
     found_vendor_wine = find_by(slug: attrs[:slug], website: attrs[:website])
